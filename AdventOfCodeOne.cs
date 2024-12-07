b7944ac26298787fcd98338337f4fff9b56830f0
FadedFilipGjorgiev77 <f.gjorgiev@students.htl-leonding.ac.at>
2024-12-07 11:15:27 +0100

    Updated file paths to actually work and converted the list to an array of ints

--- AdventOfCodeOne.cs
+++ AdventOfCodeOne.cs
@@ -1,28 +1,14 @@
 using System;
-int smallestNumberLeft = 0;
-int smallestNumberRight = 0;
-int difference = 0;
-int[] list = {2, 3, 4, 5, 6, 7 ,9, 10, 15, 25, 40};
-int i = 0;
+using System.IO;
+using System.Linq;
 
-string leftList = "LeftList.csv";
-string rightList = "RightList.csv";
+        // File paths for the CSV files
+string leftPath = "C:\\HTL\\CABS\\ProtokollAdventOfCode\\LeftList.csv"; 
+string rightPath = "C:\\HTL\\CABS\\ProtokollAdventOfCode\\RightList.csv";
 
-// Read all lines from the file (each line contains a single number)
-string[] lines = File.ReadAllLines(filePath);
 
-// Create an array to store the numbers
-int[] numbers = new int[lines.Length];
+int[] leftArray = File.ReadAllLines(leftPath).Select(int.Parse).ToArray();
+int[] rightArray = File.ReadAllLines(rightPath).Select(int.Parse).ToArray();
 
-// Convert each line to an integer and store it in the array
-for (int i = 0; i < lines.Length; i++)
-{
-    if (int.TryParse(lines[i], out int number))
-    {
-        numbers[i] = number;
-    }
-    else
-    {
-        Console.WriteLine($"Invalid value: {lines[i]}");
-    }
-}
+Console.WriteLine(string.Join(", ", leftArray));
+Console.WriteLine(string.Join(", ", rightArray));
